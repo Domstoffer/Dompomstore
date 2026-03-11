7038a3260c0a61709f72fc73339c8d4baaccbe4c
Dominic Madlindl <domimadlindl58@icloud.com>
2026-03-11 03:04:05 +0100

    chore: Remove original test/dummy products from products.js so only authentic user uploads display on screen

--- products.js
+++ products.js
@@ -1,63 +1,12 @@
 const products = [
     {
-        id: "DB-01",
-        name: "DB-01",
-        price: 49,
-        currency: "USDC",
-        image: "images/DB-01.jpg",
-        sizes: ["S", "M", "L"],
-        stock: 50,
-        description: "High quality Dompom shorts. Premium fabric. Comfortable fit. Limited release."
-    },
-    {
-        id: "PS-01",
-        name: "PS-01",
-        price: 19,
-        currency: "USDC",
-        image: "images/pepe-shirt.jpg",
-        sizes: ["S", "M", "L"],
-        stock: 100,
-        description: "Minimal Dompom shirt. Limited release."
-    },
-    {
-        id: "TESTCY",
-        name: "Testcy",
-        price: 30,
-        currency: "USDC",
-        image: "images/test.jpg",
-        sizes: ["OS"],
-        stock: 0,
-        description: "Test product."
-    },
-    {
-        id: "ZYN",
-        name: "ZYN",
-        price: 22,
-        currency: "USDC",
-        image: "images/zyn.png",
-        sizes: ["OS"],
-        stock: 0,
-        description: "Lifestyle essential."
-    },
-    {
-        id: "PROD-05",
-        name: "PRODUKT 05",
-        price: 18,
-        currency: "USDC",
-        image: "https://picsum.photos/seed/p05/400/400",
-        sizes: ["OS"],
-        stock: 10,
-        description: "Classic design. Must have piece."
-    }
-,
-    {
         id: "Fartcoin Shirt",
         name: "FS-01",
         price: 29,
         currency: "USDC",
         image: "images/1773193258736-0-unisex-garment-dyed-heavyweight-t-shirt-black-back-6953cef82cf54.PNG",
         images: ["images/1773193258736-0-unisex-garment-dyed-heavyweight-t-shirt-black-back-6953cef82cf54.PNG"],
-        sizes: ["M","L","S"],
+        sizes: ["M", "L", "S"],
         stock: 100,
         description: "Fartcoin shirt das erste DTF print Video seht ihr fit und material unwichtig"
     }
